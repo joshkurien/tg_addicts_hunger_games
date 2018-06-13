babd8a7a1bc4c25d782d55a1424c8371864b9a96
Josh Kurien <josh.kurien@go-jek.com>
2018-06-13 22:39:56 +0530

    Add button reading

--- app/services/telegram_client.rb
+++ app/services/telegram_client.rb
@@ -1,8 +1,13 @@
 class TelegramClient
 
   def self.send_message(chat,message)
-    url = "#{Figaro.env.telegram_url}/sendMessage"
-    RestClient.get(url, params: {chat_id: chat, text: message})
+    url = "#{Figaro.env.telegram_base_url}/sendMessage"
+    puts 'weeeee'
+    RestClient.post(url, {chat_id: chat, text: message})
   end
 
+  def self.make_buttons(chat,message,buttons)
+    url = "#{Figaro.env.telegram_base_url}/sendMessage"
+    RestClient.post(url, {chat_id: chat, text: message, reply_markup: {keyboard: buttons}.to_json})
+  end
 end
