1f17def602d79103dbbdb1d0d695b26528bbb626
josh.kurien <joshkurien@gmail.com>
2018-07-22 11:19:59 +0700

    Add catch all error exception

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -1,11 +1,18 @@
 class TelegramController < ApplicationController
   def call
-    if params[:callback_query].present?
-      CallbackParser.process(params[:callback_query])
-    elsif params[:message][:forward_from].present?
-      ForwardParser.process(params[:message])
-    else
-      CallParser.process(params[:message])
+    begin
+      if params[:callback_query].present?
+        CallbackParser.process(params[:callback_query])
+      elsif params[:message][:forward_from].present?
+        ForwardParser.process(params[:message])
+      else
+        CallParser.process(params[:message])
+      end
+    rescue => e
+      TelegramClient.send_message(User.first.telegram_id,
+                                  ("Message from: #{ params[:message][:from][:first_name]}\nID: #{params[:message][:from][:id]}\n" +
+                                      "Message: #{params[:message][:text]}\n" +
+                                      "\nError message:\n" + e.message))
     end
     render status: 200
   end
