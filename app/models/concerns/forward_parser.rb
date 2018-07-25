bd138e0a8a2580beb7eaa8a357d3d7e292a017e0
josh.kurien <joshkurien@gmail.com>
2018-07-25 16:19:06 +0700

    Allow beta bot forwards

--- app/models/concerns/forward_parser.rb
+++ app/models/concerns/forward_parser.rb
@@ -1,8 +1,10 @@
 class ForwardParser
   WWMODERATOR_ID = 175844556
+  BETA_MODERATOR_ID = 198626752
 
   def self.process(message)
-    if message[:forward_from][:id] == WWMODERATOR_ID
+    bot_id = message[:forward_from][:id]
+    if bot_id == WWMODERATOR_ID || bot_id == BETA_MODERATOR_ID
       user = User.get_user(message[:from])
       return unless user.check_admin
 
